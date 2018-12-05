a3426e58d2e3b5e48180b468bb6f4284826623b2
g3ry89 <gery89@gmail.com>
2018-12-05 15:10:27 +0100

    working connection between backend and frontend server

--- src/App.js
+++ src/App.js
@@ -6,7 +6,25 @@ import Navbar from "./pages/Navbar/Navbar";
 
 import "./App.css";
 
+const API = 'http://localhost:8443/login';
+
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            groups: null,
+        };
+    }
+    componentDidMount() {
+        const thisRef = this;
+        fetch(API)
+            .then(response => {
+                response.json().then(function(data) {
+                    thisRef.setState({ groups: data.render.toString() })
+                });
+            } );
+    }
+
   render() {
     return (
       <Router basename="/react-auth-ui/">
