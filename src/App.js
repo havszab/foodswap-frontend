da9241a6e7ed0fb5c5adc935b0adf5b94a2e2cc9
havszab <havaldaszab@gmail.com>
2018-12-06 12:52:10 +0100

    login and reg styling almost done

--- src/App.js
+++ src/App.js
@@ -4,52 +4,13 @@ import SignUpForm from "./pages/SignUpForm";
 import SignInForm from "./pages/SignInForm";
 import Navbar from "./pages/Navbar/Navbar";
 
-import "./App.css";
-
 class App extends Component {
+  state = {};
   render() {
     return (
-      <Router basename="/react-auth-ui/">
-        <div className="App">
-          <div className="App__Aside" />
-          <div className="App__Form">
-            <div className="PageSwitcher">
-              <NavLink
-                to="/sign-in"
-                activeClassName="PageSwitcher__Item--Active"
-                className="PageSwitcher__Item"
-              >
-                Sign In
-              </NavLink>
-              <NavLink
-                exact
-                to="/"
-                activeClassName="PageSwitcher__Item--Active"
-                className="PageSwitcher__Item"
-              >
-                Sign Up
-              </NavLink>
-            </div>
-
-            <div className="FormTitle">
-              <NavLink
-                to="/sign-in"
-                activeClassName="FormTitle__Link--Active"
-                className="FormTitle__Link"
-              >
-                Sign In
-              </NavLink>{" "}
-              or{" "}
-              <NavLink
-                exact
-                to="/"
-                activeClassName="FormTitle__Link--Active"
-                className="FormTitle__Link"
-              >
-                Sign Up
-              </NavLink>
-            </div>
-
+      <Router basename="/">
+        <div>
+          <div>
             <Route exact path="/" component={SignUpForm} />
             <Route path="/sign-in" component={SignInForm} />
             <Route path="/list-foods" component={Navbar} />
